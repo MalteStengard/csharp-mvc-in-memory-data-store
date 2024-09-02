6ce28f4d208fae8c2e3dda61eacf879bc7cad156
Malte <malte.stengard@gmail.com>
2024-09-02 13:31:54 +0200

    All working except remove

--- exercise.wwwapi/Repository/ProductRepository.cs
+++ exercise.wwwapi/Repository/ProductRepository.cs
@@ -12,28 +12,46 @@ namespace exercise.wwwapi.Repository
         }
         public Product AddProduct(Product product)
         {
-            return ProductCollection.AddProduct(product);
+            //return ProductCollection.AddProduct(product);
+            _db.Add(product);
+            _db.SaveChanges();
+            return product;
         }
 
         public Product DeleteProduct(int id)
         {
-            return ProductCollection.DeleteProduct(id);
+            //return ProductCollection.DeleteProduct(id);
+            var product = _db.Products.FirstOrDefault(p => p.Id == id) as Product;
+           _db.Remove(product);
+            return product;
 
         }
 
         public List<Product> GetAll(string category)
         {
-            return ProductCollection.GetProducts(category);
+            //return ProductCollection.GetProducts(category);
+            return _db.Products.ToList();
         }
 
         public Product GetProduct(int id)
         {
-            return ProductCollection.GetProduct(id);
+            //return ProductCollection.GetProduct(id);
+            var product = _db.Products.FirstOrDefault(p => p.Id == id) as Product;
+            return product;
         }
 
         public Product UpdateProduct(int id, Product product)
         {
-            return ProductCollection.UpdateProduct(id, product);
+            //return ProductCollection.UpdateProduct(id, product);
+            Product thisProduct = _db.Products.FirstOrDefault(p => p.Id == id);
+            if (thisProduct != null)
+            {
+                _db.Entry(thisProduct).Property(p=> p.Name).CurrentValue = product.Name;
+                _db.Entry(thisProduct).Property(p=> p.Category).CurrentValue = product.Category;
+                _db.Entry(thisProduct).Property(p=> p.Price).CurrentValue = product.Price;
+            }
+            _db.SaveChanges();
+            return thisProduct;
         }
     }
 }
